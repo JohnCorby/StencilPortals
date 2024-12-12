9e236867fcf6ce7080df3cc09f5b2c3e645160e1
JohnCorby <williamforgames6@gmail.com>
2024-12-12 01:17:40 -0800

    start custom srp

---
+++ Assets/CustomSRP/CustomRenderPipeline.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CustomRenderPipeline : RenderPipeline
+{
+	private readonly CustomRenderPipelineAsset _asset;
+
+	public CustomRenderPipeline(CustomRenderPipelineAsset asset)
+	{
+		_asset = asset;
+	}
+
+	protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ Assets/CustomSRP/CustomRenderPipelineAsset.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+[CreateAssetMenu(menuName = "Rendering/CustomRenderPipelineAsset", fileName = "CustomRenderPipelineAsset")]
+public class CustomRenderPipelineAsset : RenderPipelineAsset<CustomRenderPipeline>
+{
+	protected override RenderPipeline CreatePipeline()
+	{
+		return new CustomRenderPipeline(this);
+	}
+}
