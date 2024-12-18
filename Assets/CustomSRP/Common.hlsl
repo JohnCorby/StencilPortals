f15013b4338914f5672dc609852756ae15186af5
JohnCorby <williamforgames6@gmail.com>
2024-12-18 11:57:40 -0800

    srp batcher compatible

--- Assets/CustomSRP/Common.hlsl
+++ Assets/CustomSRP/Common.hlsl
@@ -1,14 +1,18 @@
 ﻿#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl"
 
+CBUFFER_START(UnityPerDraw)
 float4x4 unity_ObjectToWorld;
 float4x4 unity_WorldToObject;
+float4 unity_LODFade;
+real4 unity_WorldTransformParams;
+CBUFFER_END
+
 float4x4 unity_MatrixVP;
 float4x4 unity_MatrixV;
 float4x4 unity_MatrixInvV;
 float4x4 unity_prev_MatrixM;
 float4x4 unity_prev_MatrixIM;
 float4x4 glstate_matrix_projection;
-real4 unity_WorldTransformParams;
 
 #define UNITY_MATRIX_M unity_ObjectToWorld
 #define UNITY_MATRIX_I_M unity_WorldToObject
