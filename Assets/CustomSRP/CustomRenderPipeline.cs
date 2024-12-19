02cf1636970e95927da76dc81477773eb56e5f92
JohnCorby <williamforgames6@gmail.com>
2024-12-18 17:07:23 -0800

    mrt working

--- Assets/CustomSRP/CustomRenderPipeline.cs
+++ Assets/CustomSRP/CustomRenderPipeline.cs
@@ -59,7 +59,9 @@ public class CustomRenderPipeline : RenderPipeline
 		var rt1 = Shader.PropertyToID("_NormalBuffer");
 		var rt2 = Shader.PropertyToID("_DepthBuffer");
 		cmd.GetTemporaryRT(rt0, camera.pixelWidth, camera.pixelHeight, 32, FilterMode.Bilinear, GraphicsFormat.R16G16B16A16_SFloat, 8);
-		cmd.SetRenderTarget(colors: new RenderTargetIdentifier[] { rt0 }, depth: rt0);
+		cmd.GetTemporaryRT(rt1, camera.pixelWidth, camera.pixelHeight, 0, FilterMode.Bilinear, GraphicsFormat.R16G16B16A16_SFloat, 8);
+		cmd.GetTemporaryRT(rt2, camera.pixelWidth, camera.pixelHeight, 0, FilterMode.Bilinear, GraphicsFormat.R16G16B16A16_SFloat, 8);
+		cmd.SetRenderTarget(colors: new RenderTargetIdentifier[] { rt0, rt1, rt2 }, depth: rt0);
 
 		cmd.ClearRenderTarget(true, true, Color.white);
 
@@ -79,8 +81,10 @@ public class CustomRenderPipeline : RenderPipeline
 			cmd.DrawRendererList(context.CreateGizmoRendererList(camera, GizmoSubset.PostImageEffects));
 		}
 
-		cmd.Blit(rt0, BuiltinRenderTextureType.CameraTarget, _asset.PostProcessMaterial);
+		cmd.Blit(BuiltinRenderTextureType.None, BuiltinRenderTextureType.CameraTarget, _asset.PostProcessMaterial);
 		cmd.ReleaseTemporaryRT(rt0);
+		cmd.ReleaseTemporaryRT(rt1);
+		cmd.ReleaseTemporaryRT(rt2);
 
 		cmd.EndSample(sampleName);
 
